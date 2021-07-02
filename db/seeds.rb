95384a297d1149e77c2a27fb2b5bb9203fb0eae1
alexey_razuvaev <r125644@gmail.com>
2021-07-02 14:28:04 +0300

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Post.destroy_all
+Section.destroy_all
+
+FactoryBot.create(:post)
+
+parent_post = FactoryBot.create(:post)
+
+2.times { FactoryBot.create(:post, parent: parent_post) }
+
+section = FactoryBot.create(:section, name: Faker::University.suffix, post: parent_post)
+FactoryBot.create(:post, parent: parent_post, section: section)
+
+section2 = FactoryBot.create(:section, name: Faker::University.suffix, post: parent_post)
+2.times { FactoryBot.create(:post, parent: parent_post, section: section2) }
+
+puts "Path to one of the generated posts: /#{Post.parents.last.slug}"
